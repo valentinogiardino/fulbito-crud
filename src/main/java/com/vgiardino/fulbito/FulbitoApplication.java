7da1419a3ada57aef29a4c90a8feacdc05cb5fd6
Valentino Giardino <valentinogiardino17@gmail.com>
2024-05-31 16:06:37 -0300

    Initial commit

---
+++ src/main/java/com/vgiardino/fulbito/FulbitoApplication.java
@@ -0,0 +1,13 @@
+package com.vgiardino.fulbito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FulbitoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FulbitoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vgiardino/fulbito/FulbitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vgiardino.fulbito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FulbitoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
